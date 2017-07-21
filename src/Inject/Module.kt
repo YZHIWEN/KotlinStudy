bb2dad305aeb6f7deedb8071657a74ca2d094693
Dazz <1425353753@qq.com>
2017-07-21 17:34:46 +0800

    some

--- src/Inject/Module.kt
+++ src/Inject/Module.kt
@@ -3,20 +3,44 @@ package Inject
 /**
  * Created by yangzhiwen on 17/7/21.
  */
-fun <T> T.provide(init: T.() -> Unit) = apply(init)
+//fun <T> T.provide(init: T.() -> Unit) = apply(init)
 
-open class Module {
+class A
+
+abstract class Module {
 
     fun addProperty(key: String, value: String) {
         println("add property {$key : $value}")
     }
 
+    fun <T> addProperty(t: T) {
+        println("add property {$t}")
+    }
+
+    abstract fun onCreate()
 }
 
-class MyModule : Module()
+fun Module.provide(init: Module.() -> Unit) = run(init)
+
+//fun <T> T.inject(key: String): T {
+//
+//}
+
+class MyModule : Module() {
+    override fun onCreate() =
+            provide {
+                //...
+                //...
+                addProperty(A())
+            }
+}
+
+class Activity {
+//    val tag by inject()
+}
+
 
 fun main(args: Array<String>) {
-    MyModule().provide {
-        addProperty("tag", "dazz")
-    }
+    val myModule = MyModule()
+    myModule.onCreate()
 }
